8076ddfbefa8d110f085e84fd297259ba2a92221
YourBoyRory <yourboyrory@proton.me>
2026-04-02 21:29:45 -0400

    fixed service

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-make
+make || make -C /lib/modules/$(uname -r)/build \
+  M=$PWD \
+  CC=clang \
+  LD=ld.lld \
+  AR=llvm-ar \
+  NM=llvm-nm \
+  STRIP=llvm-strip \
+  OBJCOPY=llvm-objcopy \
+  modules
 sudo mkdir -p /opt/steamdeck_hwmon_shim
 sudo cp ./* /opt/steamdeck_hwmon_shim/
 sudo chmod +x /opt/steamdeck_hwmon_shim/run.sh
