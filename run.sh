d2be9c0a0a47518ba6dc6f93dc309d74ee57c448
YourBoyRory <yourboyrory@proton.me>
2026-04-15 18:40:06 -0400

    run script will update mod if it fails to run

--- run.sh
+++ run.sh
@@ -7,4 +7,8 @@ fan_addr="fan1_input"
 path=$(dirname $(grep $driver /sys/class/hwmon/*/name | awk -F ":" '{print $1}'))
 ls $path/$fan_addr
 echo "Mapping $path/$fan_addr to [steamdeck_hwmon]/fan1_input..."
-sudo insmod /opt/steamdeck_hwmon_shim/steamdeck_hwmon_shim.ko real_path=$path/$fan_addr
+sudo insmod /opt/steamdeck_hwmon_shim/steamdeck_hwmon_shim.ko real_path=$path/$fan_addr || (
+    cd /opt/steamdeck_hwmon_shim
+    /opt/install.sh
+    sudo insmod /opt/steamdeck_hwmon_shim/steamdeck_hwmon_shim.ko real_path=$path/$fan_addr
+)
